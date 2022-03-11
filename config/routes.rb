ae9d1c78544092ef24dde9788ac5f407017021b9
Luiz Fernando Alvarenga <luizfca7@gmail.com>
2022-03-11 11:51:56 -0300

    Followers implemented/testing

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resource :my_profiles, only: [ :edit, :update, :show ]
+  resource :my_profiles, only: [ :index, :edit, :update, :show ] do
+    member do
+      post :follow
+      post :unfollow
+    end
+  end
 
   resources :profiles, only: [ :index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "dashboard", to: "pages#dashboard", as: :dashboard
   resources :lessons
-  
+
 end
