9b33e0ffbed1da718172c4b058f42cd51ee80aa2
Tamara Menezes <tamaramenezes@protonmail.com>
2022-03-21 14:38:23 -0300

    landing page done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
   def after_sign_in_path_for(resource)
     lessons_path
   end
